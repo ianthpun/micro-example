e8686f0d5f97a22e144c966b7f7ddf7efa35559e
Ian Pun <ian.pun@dapperlabs.com>
2020-01-11 14:40:42 -0800

    init

---
+++ cmd/greeter/client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	proto "github.com/ianthpun/micro-example/proto"
+	micro "github.com/micro/go-micro"
+)
+
+func main() {
+	// Create a new service
+	service := micro.NewService(micro.Name("greeter.client"))
+	// Initialise the client and parse command line flags
+	service.Init()
+
+	// Create new greeter client
+	greeter := proto.NewGreeterService("greeter", service.Client())
+
+	// Call the greeter
+	rsp, err := greeter.Hello(context.Background(), &proto.Request{Name: "John"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Print response
+	fmt.Println(rsp.Msg)
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/ianthpun/micro-example
+
+go 1.13
+
+require (
+	github.com/golang/mock v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/google/btree v1.0.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.2 // indirect
+	github.com/micro/go-micro v1.18.0
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+)
