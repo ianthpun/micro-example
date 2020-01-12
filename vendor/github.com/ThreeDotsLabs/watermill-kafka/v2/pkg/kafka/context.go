5cdb0c11799b226969f01bd0ce18d49c7d6531a3
Ian Pun <ian.pun@dapperlabs.com>
2020-01-12 14:58:30 -0800

    add vendor

---
+++ vendor/github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka/context.go
@@ -0,0 +1,45 @@
+package kafka
+
+import (
+	"context"
+	"time"
+)
+
+type contextKey int
+
+const (
+	_ contextKey = iota
+	partitionContextKey
+	partitionOffsetContextKey
+	timestampContextKey
+)
+
+func setPartitionToCtx(ctx context.Context, partition int32) context.Context {
+	return context.WithValue(ctx, partitionContextKey, partition)
+}
+
+// MessagePartitionFromCtx returns Kafka partition of the consumed message
+func MessagePartitionFromCtx(ctx context.Context) (int32, bool) {
+	partition, ok := ctx.Value(partitionContextKey).(int32)
+	return partition, ok
+}
+
+func setPartitionOffsetToCtx(ctx context.Context, offset int64) context.Context {
+	return context.WithValue(ctx, partitionOffsetContextKey, offset)
+}
+
+// MessagePartitionOffsetFromCtx returns Kafka partition offset of the consumed message
+func MessagePartitionOffsetFromCtx(ctx context.Context) (int64, bool) {
+	offset, ok := ctx.Value(partitionOffsetContextKey).(int64)
+	return offset, ok
+}
+
+func setMessageTimestampToCtx(ctx context.Context, timestamp time.Time) context.Context {
+	return context.WithValue(ctx, timestampContextKey, timestamp)
+}
+
+// MessageTimestampFromCtx returns Kafka internal timestamp of the consumed message
+func MessageTimestampFromCtx(ctx context.Context) (time.Time, bool) {
+	timestamp, ok := ctx.Value(timestampContextKey).(time.Time)
+	return timestamp, ok
+}
